188984d30a70bfe20899462faa0e988d81c21f32
Toplipton <temitopeoluwafemi95@gmail.com>
2025-06-22 16:14:01 +0100

    initial commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,15 +1,12 @@
+function Button({ onClick, children, className }) {
+  return (
+    <button
+      onClick={onClick}
+      className={`px-4 py-2 text-lg font-semibold rounded-xl focus:outline-none shadow-sm active:scale-95 transition duration-150 ease-in-out ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
 
-
-function Button({onClick, children, className}) {
-    return (
-      <button
-        onClick={onClick}
-        className={`px-4 py-2 text-lg font-semibold rounded-xl focus:outline-none shadow-sm active:scale-95 transition duration-150 ease-in-out ${className}`}
-      >
-        {children}
-      </button>
-    );
-  }
-
-  export default Button;
-  
+export default Button;
